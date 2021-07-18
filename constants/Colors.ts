439a43805a0bcfedeee6c16c13e563907fbf53c3
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-18 22:00:34 +0000

    modify colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,14 +4,14 @@ const tintColorDark = '#fff';
 export default {
   light: {
     text: '#000',
-    background: '#fff',
+    background: '#e8f4f8',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    tabIconSelected: "#020079",
   },
   dark: {
     text: '#fff',
-    background: '#000',
+    background: '#e8f4f8',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
