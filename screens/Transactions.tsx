0297a20b0701194d3f83b3fe90dd9fd354586971
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-18 21:57:58 +0000

    add screens

---
+++ screens/Transactions.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { Text, View } from '../components/Themed';
+
+export default function Transactions() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
