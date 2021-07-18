a50769ab82f8d917e1b98ca278632043c5bad5b7
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-18 22:01:39 +0000

    extra changes

---
+++ screens/index.js
@@ -0,0 +1,5 @@
+export { Home as default } from './Home' 
+export { Profile as default } from './Profile' 
+export { Payment as default } from './Payment' 
+export { Transactions as default } from './Transactions' 
+export { Expenses as default } from './CreateExpense' 
