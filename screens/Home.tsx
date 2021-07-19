0cf191f1b9945b8dcb1a8cc991ac71b280d99e22
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-19 00:38:29 +0000

    add home

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet,SafeAreaView,StatusBar } from 'react-native';
 import { Text, View } from '../components/Themed';
+import Card from '../components/Card';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      
+        <View style={styles.cardContainer}>
+            <Card/>
+        </View>
+        <View style={styles.overviewContainer}>
+
+        </View>
     </View>
   );
 }
@@ -13,8 +19,15 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  
+  cardContainer:{
+    flex:0.55,
+    // backgroundColor:'red'
+  },
+  overviewContainer:{
+    flex:0.45,
+    // backgroundColor:'blue'
+  }
 });
