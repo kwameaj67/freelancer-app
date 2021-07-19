c9589aae4c7ab14b9a38ca70a84601404ef1c2a8
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-19 10:01:36 +0000

    add auth flow

--- types.tsx
+++ types.tsx
@@ -2,7 +2,9 @@
  * Learn more about using TypeScript with React Navigation:
  * https://reactnavigation.org/docs/typescript/
  */
-
+export type AutheticationStackParamList = {
+  Login:undefined,
+}
 export type RootStackParamList = {
   Root: undefined;
   NotFound: undefined;
